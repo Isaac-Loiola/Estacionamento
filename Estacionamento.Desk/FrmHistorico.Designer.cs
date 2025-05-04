18d89d62dcdc699d3de5a83cb9aaeafdc42cda1c
Isaac Loiola <isaackopi@icloud.com>
2025-05-04 19:55:13 -0300

    Coluna renomeada no dgvHistorico

--- Estacionamento.Desk/FrmHistorico.Designer.cs
+++ Estacionamento.Desk/FrmHistorico.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHistorico));
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             panel1 = new Panel();
             dgvHistorico = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
-            Column5 = new DataGridViewTextBoxColumn();
             panel3 = new Panel();
             btnHistorico = new Button();
             btnVeiculo = new Button();
             btnEntradaSaida = new Button();
             panel4 = new Panel();
             label1 = new Label();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvHistorico).BeginInit();
             panel3.SuspendLayout();
@@ -63,18 +63,18 @@
             // 
             dgvHistorico.AllowUserToAddRows = false;
             dgvHistorico.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle3.Font = new Font("Mont Heavy DEMO", 9F, FontStyle.Bold);
-            dgvHistorico.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.Font = new Font("Mont Heavy DEMO", 9F, FontStyle.Bold);
+            dgvHistorico.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             dgvHistorico.BackgroundColor = Color.White;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle4.BackColor = SystemColors.Control;
-            dataGridViewCellStyle4.Font = new Font("Mont Heavy DEMO", 9F, FontStyle.Bold);
-            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = Color.Transparent;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            dgvHistorico.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Mont Heavy DEMO", 9F, FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.Transparent;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvHistorico.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dgvHistorico.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvHistorico.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
             dgvHistorico.Location = new Point(21, 22);
@@ -83,40 +83,6 @@
             dgvHistorico.RowHeadersVisible = false;
             dgvHistorico.Size = new Size(962, 453);
             dgvHistorico.TabIndex = 1;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "ID";
-            Column1.Name = "Column1";
-            Column1.ReadOnly = true;
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "Data de entrada";
-            Column2.Name = "Column2";
-            Column2.ReadOnly = true;
-            Column2.Width = 250;
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "Data de Saida";
-            Column3.Name = "Column3";
-            Column3.ReadOnly = true;
-            Column3.Width = 209;
-            // 
-            // Column4
-            // 
-            Column4.HeaderText = "Data de Entrada";
-            Column4.Name = "Column4";
-            Column4.ReadOnly = true;
-            Column4.Width = 250;
-            // 
-            // Column5
-            // 
-            Column5.HeaderText = "Valor";
-            Column5.Name = "Column5";
-            Column5.ReadOnly = true;
-            Column5.Width = 150;
             // 
             // panel3
             // 
@@ -186,6 +152,40 @@
             label1.TabIndex = 19;
             label1.Text = "Historico";
             // 
+            // Column1
+            // 
+            Column1.HeaderText = "ID";
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Data de entrada";
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 250;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Data de Saida";
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 209;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Status";
+            Column4.Name = "Column4";
+            Column4.ReadOnly = true;
+            Column4.Width = 250;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Valor";
+            Column5.Name = "Column5";
+            Column5.ReadOnly = true;
+            Column5.Width = 150;
+            // 
             // FrmHistorico
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -217,11 +217,11 @@
         private Button btnEntradaSaida;
         private Panel panel4;
         private DataGridView dgvHistorico;
+        private Label label1;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
-        private Label label1;
     }
 }
