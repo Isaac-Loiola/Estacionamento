b91b559774f98e618e43d1430df9e0d97b4b0bf3
Isaac Loiola <isaackopi@icloud.com>
2025-05-06 08:46:06 -0300

    Atualizando a pasta BancoDeDados!

--- BancoDeDados/EstacionamentoDB.sql
+++ BancoDeDados/EstacionamentoDB.sql
@@ -1,65 +1,20 @@
-CREATE DATABASE  IF NOT EXISTS `estacionamentodb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `estacionamentodb`;
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: estacionamentodb
--- ------------------------------------------------------
--- Server version	9.1.0
+CREATE TABLE movimentacoes (
+  id int NOT NULL AUTO_INCREMENT,
+  id_veiculo int DEFAULT NULL,
+  data_entrada timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  data_saida timestamp NULL DEFAULT NULL,
+  situacao bit(1) DEFAULT NULL,
+  valor decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (id),
+  foreign key(id_veiculo) references veiculos(id)
+) ;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `movimentacoes`
---
 
-DROP TABLE IF EXISTS `movimentacoes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `movimentacoes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_veiculo` int DEFAULT NULL,
-  `data_entrada` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `data_saida` timestamp NULL DEFAULT NULL,
-  `situacao` bit(1) DEFAULT NULL,
-  `valor` decimal(10,0) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_veiculo` (`id_veiculo`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `veiculos`
---
-
-DROP TABLE IF EXISTS `veiculos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `veiculos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `placa` varchar(7) NOT NULL,
-  `modelo` varchar(12) NOT NULL,
-  `tipo_veiculo` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `placa` (`placa`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-05-04 21:45:53
+CREATE TABLE veiculos (
+  id int NOT NULL AUTO_INCREMENT,
+  placa varchar(7) NOT NULL,
+  modelo varchar(12) NOT NULL,
+  tipo_veiculo int NOT NULL,
+  PRIMARY KEY (id) 
+) ;
